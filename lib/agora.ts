7cdc9efdbebb135b82aa26dacc3462392d9904e9
Sumato Ken <bermime1@gmail.com>
2022-08-30 11:46:01 +0100

    added Agora

---
+++ lib/agora.ts
@@ -0,0 +1,15 @@
+export let rtc = {
+    localAudioTrack: null,
+    client: null
+};
+
+export let options = {
+    // Pass your App ID here.
+    appId: process.env.AGORA_APP_ID,
+    // Set the channel name.
+    channel: "first",
+    // Pass your temp token here.
+    token: "007eJxTYBBNd9NKSpvCEzX/B0fghTLBqp0pEy02daUcbonwOZ9ufE+BwdLYyCTV0iItJdXE0iQtxTLJxNA0zczIwCg1MSkt1cDQ+iVv8m4r/uRSPXYmRgYIBPFZGdIyi4pLGBgA27seog==",
+    // Set the user ID.
+    uid: 123456
+};
---
+++ pages/call/club.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Club() {
+  return <div>club</div>;
+}
